ae6c8672a07ef62c57a835a32a3b83cdbff9daa5
Jem Young <jemuel.young@gmail.com>
2014-02-27 23:45:42 -0500

    Add main pages

---
+++ main/gulpfile.js
@@ -0,0 +1,37 @@
+// Include gulp
+var gulp = require('gulp'); 
+
+// Include Our Plugins
+var jshint = require('gulp-jshint');
+var less = require('gulp-less');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+// Lint Task
+gulp.task('lint', function() {
+	 return gulp.src('js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'))
+        .pipe(jshint.reporter('fail'));
+        deferred.resolve();
+});
+
+// Compile Our Less
+gulp.task('less', function() {
+    return gulp.src('less/*.less')
+        .pipe(less())
+        .pipe(gulp.dest('public/media/css'));
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src('js/*.js')
+        .pipe(concat('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('public/media'));
+});
+
+
+// Default Task
+gulp.task('default', ['less', 'scripts']);
